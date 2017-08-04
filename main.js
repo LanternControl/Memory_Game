496da5018e0c55de039d6420ab42d2977d4a039c
LanternControl <brett4president@hotmail.com>
2017-08-04 15:38:43 -0400

    changed vars to something better. working on flipping cards

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let mem_array = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J'];
+//Be sure to change this to something way more cool than letters.
+
 let mem_values = [];
-let mem_tile = [];
-let flipped_tiles = 0;
+let mem_card = [];
+let flipped_cards = 0;
 
 // Shuffle function by Adam Khoury
-Array.prototype.memory_tile_shuffle = function(){
+Array.prototype.memory_card_shuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
         j = Math.floor(Math.random() * (i+1));
@@ -16,15 +18,26 @@ Array.prototype.memory_tile_shuffle = function(){
 
 // for new game, also creates divs for individual tiles
 function newGame(){
-    flipped_tiles = 0;
+    flipped_cards = 0;
     let output = '';
-    mem_array.memory_tile_shuffle();
+    mem_array.memory_card_shuffle();
     for (let i=0; i < mem_array.length; i++){
-        output += '<div id="tile_'+i+'" onclick="flipTile(this,\''+mem_array[i]+'\')"></div>';
+        output += '<div id="card_'+i+'" onclick="flipCard(this,\''+mem_array[i]+'\')"></div>';
     }
     document.getElementById("memory_board").innerHTML = output;
 }
 
-
 //Had to add event listener so loads on startup
 window.addEventListener("load", newGame());
+
+function flipCard(card,val){
+    if(card.innerHTML == "" && mem_values.length < 2){
+        card.style.background = "green"; //put pictures here
+        card.innerHTML = val;
+        if(mem_values.length == 0){
+            mem_values.push(val);
+            mem_card
+        }
+        
+    }
+}
